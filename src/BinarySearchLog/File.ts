f11c904d678056c3cec09659e39ce6b3ae3d54fc
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-08-29 11:29:06 +0100

    Exclude files which have only out-of-range lines

--- src/BinarySearchLog/File.ts
+++ src/BinarySearchLog/File.ts
@@ -33,14 +33,21 @@ export class File {
 
     /**
      *
+     * @param position
      */
-    private async readSubsequentLine() {
+    private async readSubsequentLine(position: number | null = null) {
+        let nextPosition: number | null
+        if(position !== null) {
+            this.currentBlockLines = []
+            this.currentPartialLine = ""
+        }
+        nextPosition = position
         if(!this.currentBlockLines.length) {
             const read = util.promisify(fs.read)
             const chunkSize = 65536
             const buffer = Buffer.alloc(chunkSize)
             do {
-                const result = await read(this.filehandle, buffer, 0, chunkSize, null)
+                const result = await read(this.filehandle, buffer, 0, chunkSize, nextPosition)
                 if(result.bytesRead == 0) {
                     if(this.currentPartialLine.length) {
                         const line = this.currentPartialLine
@@ -52,6 +59,10 @@ export class File {
                 }
                 const contents = this.currentPartialLine + buffer.toString("utf8", 0, result.bytesRead)
                 const lines = contents.split(/\n/)
+                if(nextPosition) {
+                    nextPosition = null
+                    lines.shift()
+                }
                 this.currentPartialLine = lines.pop() || ""
                 this.currentBlockLines = lines
             } while(this.currentBlockLines.length == 0)
@@ -63,17 +74,48 @@ export class File {
      *
      */
     async read() {
-        let line: string | null
-        do {
-            line = await this.readSubsequentLine()
-            if(line !== null) {
-                const relativePosition = this.lineCheck(line)
-                if(relativePosition > 0) {
-                    return
-                } else if(relativePosition == 0) {
-                    process.stdout.write(line + "\n")
+        if(this.firstLineIsInRange() && this.lastLineIsInRange()) {
+            let line: string | null
+            do {
+                line = await this.readSubsequentLine()
+                if(line !== null) {
+                    const relativePosition = this.lineCheck(line)
+                    if(relativePosition > 0) {
+                        return
+                    } else if(relativePosition == 0) {
+                        process.stdout.write(line + "\n")
+                    }
                 }
-            }
-        } while(line !== null)
+            } while(line !== null)
+        }
+    }
+
+    private async firstLineIsInRange() {
+        const line = await this.readSubsequentLine(0)
+        if(line !== null) {
+            const relativePosition = this.lineCheck(line)
+            return relativePosition <= 0
+        } else {
+            return false
+        }
+    }
+
+    private async lastLineIsInRange() {
+        const lastLineEstimatedLength = 1024
+        const lastLineOffset = lastLineEstimatedLength
+        let secondToLastLine: string | null = null
+        const stat = fs.fstatSync(this.filehandle)
+        const fileLength = stat.blocks * stat.blksize
+        while(secondToLastLine === null && lastLineOffset < fileLength) {
+            secondToLastLine = await this.readSubsequentLine(fileLength - lastLineOffset)
+        }
+        if(secondToLastLine !== null) {
+            let lastLine: string | null = null
+            do {
+                lastLine = await this.readSubsequentLine()
+            } while(lastLine === null)
+            const relativePosition = this.lineCheck(lastLine)
+            return relativePosition >= 0
+        }
     }
 }
