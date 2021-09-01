2e104652005171d9bd1e1886e9f841f9a03c330a
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-09-01 23:43:41 +0100

    More helpful bin script

--- bin.ts
+++ bin.ts
@@ -1,8 +1,17 @@
-import { Filename } from "./index"
+import { EOLPattern, Filename, DateSearcher, LineDatePattern } from "./index"
 
+const [filename, lowString, highString] = process.argv.slice(2)
+
+const low = new Date(lowString)
+const high = new Date(highString)
+const dateSearcher = new DateSearcher(
+    LineDatePattern.Syslog,
+    (dateIn) => dateIn + " " + low.getFullYear()
+)
 const file = new Filename(
-    line => line.match(/process[.]/) ? 1 : 0,
-    process.argv[2]
+    dateSearcher.lineHandler(low, high),
+    filename,
+    EOLPattern.FoldedLine
 )
 const getLines = async () => {
     for await (const line of file.read()) {
