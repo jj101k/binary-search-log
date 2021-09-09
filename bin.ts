e2d67ea647a88256c7dea380c7e0ca129753778e
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-09-09 21:56:41 +0100

    Add a help message

--- bin.ts
+++ bin.ts
@@ -24,9 +24,10 @@ const options = getopts(process.argv.slice(2), {
       "after-date": "a",
       "before-date": "b",
       "format": "f",
+      "help": "h",
       "sample-consumer": "s"
     },
-    boolean: ["s"],
+    boolean: ["h", "s"],
     default: {
         "f": "dateAutodetect"
     },
@@ -38,6 +39,58 @@ const format = options["format"]
 const lowString = options["after-date"]
 const highString = options["before-date"]
 const sampleConsumer = options["sample-consumer"]
+
+function help() {
+    process.stderr.write(`
+Usage: ${process.argv[1]} [--after-date DATE] [--before-date DATE]
+    [--format FORMAT] [--help] [-s] FILENAME...
+
+Finds log file lines between the supplied before and after dates.
+
+    --after-date        -a  Sets the date after which lines will be
+                            rejected. Date formats that Javascript can parse
+                            are required, and ISO8601 (eg.
+                            1999-12-31T23:59:59Z) is probably the easiest.
+
+    --before-date       -b  Sets the date before which lines will be
+                            rejected. The format is the same as --after-date
+                            above.
+
+    --format            -f  The format of the log line in terms of date
+                            discovery. The default is "dateAutodetect", which
+                            should work for most cases (by picking one of the
+                            others). The handlers are:
+
+                                commonLogFormat
+                                    Access logs in CLF format
+                                dateAutodetect
+                                    Detect which other handler to use
+                                dateAutodetectPerLine
+                                    As dateAutodetect, but does its thing on
+                                    every line. Use if you have a mix of
+                                    different line formats in there.
+                                syslog
+                                    Normal syslog format, eg. /var/log/messages
+                                universalSortableLog
+                                    Lines starting with a date like
+                                    "1999-12-31 23:59:59"
+
+    --help              -h  This message
+
+    --sample-consumer   -s  An example consumer of the line data. This isn't
+                            intended for general use, and is instead present
+                            as a reference for people who want to use this
+                            package's API directly.
+    `.replace(/^\s+/g, "").replace(/\s+$/, "\n\n"))
+}
+
+if(options["help"]) {
+    help()
+    process.exit(0)
+} else if(!filenames.length) {
+    help()
+    process.exit(1)
+}
 
 const binarySearchTester = Factory.getBinarySearchDateTester(format)
 const binarySearchTesterInstance = new binarySearchTester(
