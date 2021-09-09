fa37aa1dd06ff39d50595835ea5f85a6f4fb9d69
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-09-09 19:18:24 +0100

    Test a large selection of random-length lines

--- test/BinarySearchTester/Syslog.test.ts
+++ test/BinarySearchTester/Syslog.test.ts
@@ -1,4 +1,5 @@
 import assert from "assert"
+import * as fs from "fs"
 import {describe, it} from "mocha"
 import { Factory } from "../../index"
 
@@ -59,6 +60,56 @@ describe("Date searcher tests (syslog)", () => {
             }
             file.finish()
             assert.equal(seenLines, 100, "Matching lines seen")
+        })
+    })
+    describe("Can find every line in a random-line-length file", function() {
+        this.slow(500) // these have a lot of work to do
+
+        let fileHandle: number
+        before(() => fileHandle = fs.openSync(example1To100LogFile, "r"))
+        after(() => fs.closeSync(fileHandle))
+        const byteLineFinder = Factory.getLineFinder("byte")
+        it("Can find every line in a random-line-length file (by-byte)", async () => {
+            for(let i = 0; i < 100; i++) {
+                const targetDate = new Date(referenceDate.valueOf())
+                targetDate.setHours(i)
+                const file = new byteLineFinder(
+                    new binarySearchTester(targetDate, targetDate, referenceDate),
+                    example1To100LogFile,
+                    undefined,
+                    fileHandle
+                )
+                let seenLines = 0
+                for await(const line of file.readLines()) {
+                    seenLines++
+                    const parts = line.split(/ +/, 3)
+                    assert.equal(parts[1], 1 + Math.floor(i / 24))
+                    assert.equal(+parts[2].replace(/:.*/, ""), i % 24)
+                }
+                file.finish()
+                assert.equal(seenLines, 1, `Exactly one line seen (${i}) (${targetDate})`)
+            }
+        })
+        it("Can find every line in a random-line-length file (by-line)", async () => {
+            for(let i = 0; i < 100; i++) {
+                const targetDate = new Date(referenceDate.valueOf())
+                targetDate.setHours(i)
+                const file = new lineFinder(
+                    new binarySearchTester(targetDate, targetDate, referenceDate),
+                    example1To100LogFile,
+                    undefined,
+                    fileHandle
+                )
+                let seenLines = 0
+                for await(const line of file.readLines()) {
+                    seenLines++
+                    const parts = line.split(/ +/, 3)
+                    assert.equal(parts[1], 1 + Math.floor(i / 24))
+                    assert.equal(+parts[2].replace(/:.*/, ""), i % 24)
+                }
+                file.finish()
+                assert.equal(seenLines, 1, `Exactly one line seen (${i}) (${targetDate})`)
+            }
         })
     })
 })
